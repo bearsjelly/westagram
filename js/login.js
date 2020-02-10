8a1364bbf2ae84d4092d35d6be71a1c8f6c65bcd
bearsjelly <59649268+bearsjelly@users.noreply.github.com>
2020-02-10 14:23:09 +0900

    commit message

--- js/login.js
+++ js/login.js
@@ -1,20 +1,29 @@
 // 1. id, pw 에 각각 한 글자 이상 써야 버튼이 활성화 되도록 해주세요.
 // - 원래 연한 파란색이었다가 -> 활성화 되면 파란색으로!
-
+let divA = document.querySelector('.wrap');
 let idA = document.querySelector('.ID');
 let pwA = document.querySelector('.PW');
 let button = document.querySelector('.btn');
+let htmlA = document.querySelector('html');
 
 
-pwA.addEventListener('keydown',function(){
+htmlA.addEventListener('keyup',function(){
 let idB = idA.value.length; //1이상
 let pwB = pwA.value.length;  // 1이상
 
-
-    if(idB > 0 && pwB > -1){
+    if(idB > 0 && pwB > 0) {
         button.style.backgroundColor = '#5197e9';
     }
 })
+
+// idA.addEventListener('keyup',function(){
+//     let idB = idA.value.length; //1이상
+//     let pwB = pwA.value.length;  // 1이상
+    
+//         if(idB > 0 && pwB > 0) {
+//             button.style.backgroundColor = '#5197e9';
+//         }
+//     })
 
 
 // idA.addEventListener('keyup'function(){
